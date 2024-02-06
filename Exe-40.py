67d20dbbe7caf3f5d696825ac7856ed4e86bc9e3
Guilherme Carvalho <129903308+Guilherm00@users.noreply.github.com>
2024-02-06 12:42:38 -0300

    Exercícios de Python

--- Exe-40.py
+++ Exe-40.py
@@ -1,19 +1,14 @@
 #program de media escolar 40
 nota1 = float (input("insira a primeira nota:"))
 nota2 = float (input("insira a segunda nota:"))
-media = (nota1 + nota2) / 2
+media = nota1 + nota2 / 2
 
-print('sua media é:')
-print(media)
+print('sua media é:{:.1f}'.format(media))
 if media < 5 :
-  print("Parabés você foi incompetente REPROVADO")
-  print("sua média é :{}".format(media))
+  print("REPROVADO")
 elif media > 7 :
- print("Parabéns você fes o minimo e tirou a media APROVADO")
- print("sua média é :{}".format(media))
-elif media > 5  < 6.9 :
+ print("Tirou a media APROVADO")
+elif media > 5  < 7 :
   print("Olha você foi quase mediano mas vc TA DE RECUPERAÇÃO xD")
-  print("sua média é :{}".format(media))
 elif media >= 10 :
-  print('CARACA você é brabo aprovadissivo!!')
-  print("sua média é :{}".format(media))
+  print('CARACA você é foi aprovadissivo!!')
