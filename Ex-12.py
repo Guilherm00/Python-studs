c28449c0f8cb6684ad9f5a872e4db360e683156e
Guilherme Carvalho <129903308+Guilherm00@users.noreply.github.com>
2024-06-14 13:39:49 -0300

    Add files via upload

--- Ex-12.py
+++ Ex-12.py
@@ -1,4 +1,4 @@
 print ("=-=-=-=-=-=-=-=-=PROGRAMA DE DESCONTO=-=-=-=-=-=-=-=-=")
 v = float(input("Qual valor da sua compra?: "))
-d = v * (5 / 100)
-print ("O valor com 5% de desconto é {:,.2f}".format(v-d))
+d = v * 5 / 100
+print ("O valor com 5% de desconto é R${:,.2f}".format(v-d))
